a58965cfb74aace401164178de94826e92eccfca
keashynnaidoo <keashyn.naidoo@my.jcu.edu.au>
2020-09-09 16:22:58 +0800

    initial Commit Console Version

--- test_place.py
+++ test_place.py
@@ -18,8 +18,16 @@ def run_tests():
     print("Test initial-value place:")
     new_place = Place("Malagar", "Spain", 1, False)
     # TODO: Write tests to show this initialisation works
+    print("Test aleranate method for marking visited places")
+    """This is the visited value that has changed from boolean to str; oupost should still be the same"""
+    new_places=place("Malagar", "spain",1,"n")
+    print(new_places)
 
     # TODO: Add more tests, as appropriate, for each method
-
-
+    print(new_places)
+    """String is representation of place"""
+    print(new_places.check_visited())
+    """Check if place has been visited or not"""
+    print(new_places.is_important())
+    """Important that place is added to the list"""
 run_tests()
