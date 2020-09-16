5f3a0260f4a3ee3866fc83ee4cdbfc2bbcb39170
keashynnaidoo <keashyn.naidoo@my.jcu.edu.au>
2020-09-16 20:02:03 +0800

    initial Commit Gui Version

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 """
-Name:
-Date:
-Brief Project Description:
-GitHub URL:
+Name: Keashyn Naidoo
+Date: 19 September 2020
+Brief Project Description: An application that tracks places that a user has visited or unvisited
+GitHub URL:https://github.com/JCUS-CP1404/assignment-2-travel-tracker-Keashyn-naidoo
 """
-# Create your main program in this file, using the TravelTrackerApp class
+"""Create your main program in this file, using the TravelTrackerApp class"""
 
 from kivy.app import App
 
 
-class TravelTrackerApp(App):
+class TravelTrackerApp (App) :
     """..."""
     pass
 
 
-if __name__ == '__main__':
-    TravelTrackerApp().run()
+if __name__ == '__main__' :
+    TravelTrackerApp ( ).run ( )
